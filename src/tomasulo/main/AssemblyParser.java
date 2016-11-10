d76f818edcfd6d8402a9c243b727a8bd116130eb
YasmeenWafa <yasmeen_wafa@hotmail.com>
2016-11-11 00:41:17 +0200

    added instructions enums, part of parser

--- src/tomasulo/main/AssemblyParser.java
+++ src/tomasulo/main/AssemblyParser.java
@@ -1,5 +1,24 @@
 package tomasulo.main;
 
+import java.util.ArrayList;
+
 public class AssemblyParser {
 
+	
+	
+	public ArrayList parseInstruction(String s)
+	{
+		ArrayList instructionDecoded = new ArrayList();
+		
+		String[]  inst = s.split(" ");
+		instructionDecoded.add(inst[0]); //the instruction 
+		
+		String [] regs = inst[1].split(", "); //the registers
+		for(int i = 0; i<regs.length; i++)
+		{
+			instructionDecoded.add(regs[i]);
+		}
+		
+		return instructionDecoded;
+	}
 }
--- src/tomasulo/main/Instruction.java
+++ src/tomasulo/main/Instruction.java
@@ -1,5 +1,5 @@
 package tomasulo.main;
 
 public enum Instruction {
-
+	LW,SW,BEQ,NAND,ADD,ADDI,SUB,MULT,JMP,JALR,RET
 }
