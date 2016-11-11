1c2c08d7f0b4b7e4377ac5f4c11b46e590d9dd63
Nabila Ahmed <nabila.ahmed63@gmail.com>
2016-11-11 15:14:00 +0200

    ROB instance vars, getters and setters

--- src/tomasulo/main/ReorderBuffer.java
+++ src/tomasulo/main/ReorderBuffer.java
@@ -2,4 +2,38 @@ package tomasulo.main;
 
 public class ReorderBuffer {
 
+	private Instruction[] instruction;
+	private int[] destinationReg;
+	private int[] value;
+
+	public ReorderBuffer(int numOfFUs) {
+
+		instruction = new Instruction[numOfFUs];
+		destinationReg = new int[numOfFUs];
+		value = new int[numOfFUs];
+	}
+
+	public Instruction[] getInstruction() {
+		return instruction;
+	}
+
+	public void setInstruction(int index, Instruction instruction) {
+		this.instruction[index] = instruction;
+	}
+
+	public int[] getDestinationReg() {
+		return destinationReg;
+	}
+
+	public void setDestinationReg(int index, int destinationReg) {
+		this.destinationReg[index] = destinationReg;
+	}
+
+	public int[] getValue() {
+		return value;
+	}
+
+	public void setValue(int index, int value) {
+		this.value[index] = value;
+	}
 }
