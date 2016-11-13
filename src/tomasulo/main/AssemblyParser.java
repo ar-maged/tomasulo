3c0cd0b5e0596d6f349523f203914a161df0c8c2
YasmeenWafa <yasmeen_wafa@hotmail.com>
2016-11-13 20:01:18 +0200

    added some limitations to catch invalid instructions

--- src/tomasulo/main/AssemblyParser.java
+++ src/tomasulo/main/AssemblyParser.java
@@ -9,6 +9,54 @@ public class AssemblyParser {
 	static String cacheInfo;
 	static String cache2Info;
 	static int numOfInstructions;
+	ArrayList decodedInstructions;
+	ArrayList decodedCacheInfo;
+	
+	
+	
+	public AssemblyParser() //constructor
+	{
+		
+		
+		decodedInstructions = new ArrayList();
+		decodedCacheInfo = new ArrayList();
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter cache geometry");
+		cacheInfo = sc.nextLine();
+		
+		
+		decodedCacheInfo.add(parseCacheInfo(cacheInfo));
+		
+		System.out.print("Do you want another level of cache?");
+		String newcache = sc.nextLine();
+		if (newcache.equalsIgnoreCase("yes")
+				|| newcache.equalsIgnoreCase("true")) {
+			System.out.println("Please enter cache 2 geometry");
+			cache2Info = sc.nextLine();
+			decodedCacheInfo.add(parseCacheInfo(cache2Info));
+
+		}
+		System.out.println("Enter the instructions"); // one instruction format
+														// should be ex: add
+														// r1,r2,r3
+		instructions = sc.nextLine();
+		// several instructions should be ex: add r1,r2,r3 , sub r4,r5,6
+
+		String[] instructionsSeperate = instructions.split(" , ");
+
+		numOfInstructions = instructionsSeperate.length;
+
+		for (int i = 0; i < instructionsSeperate.length; i++) {
+			decodedInstructions.add(parseInstruction(instructionsSeperate[i]));
+
+		}
+
+	}
+	
+	
+	
 
 	public static int getNumOfInstructions() {
 		return numOfInstructions;
@@ -43,9 +91,40 @@ public class AssemblyParser {
 		String[] inst = s.split(" ");
 		instructionDecoded.add(inst[0]); // the instruction
 
+
+     switch(inst[0])
+     {
+     case "ADD": instructionDecoded.add(inst[0]); break;
+     case "SUB": instructionDecoded.add(inst[0]); break;
+     case "ADDI": instructionDecoded.add(inst[0]); break;
+     case "NAND":instructionDecoded.add(inst[0]); break;
+     case "LW":instructionDecoded.add(inst[0]); break;
+     case "SW":instructionDecoded.add(inst[0]); break;
+     case "MULT":instructionDecoded.add(inst[0]); break;
+     case "JMP":instructionDecoded.add(inst[0]); break;
+     case "JALR":instructionDecoded.add(inst[0]); break;
+     case "RET":instructionDecoded.add(inst[0]); break;
+     case "BEQ":instructionDecoded.add(inst[0]); break;
+     default:System.out.println("invalid instruction"); return null;
+     }
+		
+		
 		String[] regs = inst[1].split(","); // the registers
 		for (int i = 0; i < regs.length; i++) {
-			instructionDecoded.add(regs[i]);
+			
+			switch(regs[i])
+			{
+			case"R0":instructionDecoded.add(regs[i]); break;
+			case"R1":instructionDecoded.add(regs[i]); break;
+			case"R2":instructionDecoded.add(regs[i]); break;
+			case"R3":instructionDecoded.add(regs[i]); break;
+			case"R4":instructionDecoded.add(regs[i]); break;
+			case"R5":instructionDecoded.add(regs[i]); break;
+			case"R6":instructionDecoded.add(regs[i]); break;
+			case"R7":instructionDecoded.add(regs[i]); break;
+			default: System.out.println("invalid registers, only registers from 0 to 7"); return null;
+			}
+			
 		}
 
 		for (int i = 0; i < instructionDecoded.size(); i++) {
@@ -54,37 +133,9 @@ public class AssemblyParser {
 		return instructionDecoded;
 	}
 
-	public static void main(String[] args) // for testing
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter cache geometry");
-		cacheInfo = sc.nextLine();
-		parseCacheInfo(cacheInfo);
-		System.out.print("Do you want another level of cache?");
-		String newcache = sc.nextLine();
-		if (newcache.equalsIgnoreCase("yes")
-				|| newcache.equalsIgnoreCase("true")) {
-			System.out.println("Please enter cache 2 geometry");
-			cache2Info = sc.nextLine();
-			parseCacheInfo(cacheInfo);
-
-		}
-		System.out.println("Enter the instructions"); // one instruction format
-														// should be ex: add
-														// r1,r2,r3
-		instructions = sc.nextLine();
-		// several instructions should be ex: add r1,r2,r3 , sub r4,r5,6
-
-		String[] instructionsSeperate = instructions.split(" , ");
-
-		numOfInstructions = instructionsSeperate.length;
-		//System.out.println(instructionsSeperate.length);
-
-//		for (int i = 0; i < instructionsSeperate.length; i++) {
-//			parseInstruction(instructionsSeperate[i]);
-//
-//		}
-
+	public static void main(String[] args) { //for testing
+		AssemblyParser a = new AssemblyParser();
+		
 	}
 
 }
