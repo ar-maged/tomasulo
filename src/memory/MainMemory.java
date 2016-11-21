3edf4520f56c38f7120013ad92542865b252a8bc
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-11-21 04:16:36 +0200

    Add proper handlig for specifying the address in bytes

--- src/memory/MainMemory.java
+++ src/memory/MainMemory.java
@@ -12,13 +12,14 @@ public class MainMemory {
 
 	public MainMemory(int blockSizeInBytes) {
 		this.blockSizeInBytes = blockSizeInBytes;
-		this.blocks = new Block[memoryCapacity / blockSizeInBytes];
-		for (int i = 0; i < memoryCapacity / blockSizeInBytes; i++) {
+		int numberOfRows = memoryCapacity / blockSizeInBytes;
+		this.blocks = new Block[numberOfRows];
+		for (int i = 0; i < numberOfRows; i++) {
 			this.blocks[i] = new Block(blockSizeInBytes);
 		}
 	}
 
-	private Stack<String> convertInstructionsToStack(String[] instructionsArray) {
+	private Stack<String> convertInstructionsArrayToStack(String[] instructionsArray) {
 		List<String> instructionsList = Arrays.asList(instructionsArray);
 		Stack<String> instructionsStack = new Stack<>();
 		Collections.reverse(instructionsList);
@@ -27,27 +28,29 @@ public class MainMemory {
 	}
 
 	public void readProgram(String[] instructions, int startAddressInBytes) {
-		Stack<String> instructionsStack = this.convertInstructionsToStack(instructions);
+		Stack<String> instructionsStack = this.convertInstructionsArrayToStack(instructions);
 		while (!instructionsStack.isEmpty()) {
 			for (int i = 0; i < this.blockSizeInBytes / 4; i++) {
-				if (!instructionsStack.isEmpty())
-					this.blocks[(int)startAddressInBytes / 4].addData(instructionsStack.pop(), i);
+				if (!instructionsStack.isEmpty()){
+					this.blocks[(int)startAddressInBytes / this.blockSizeInBytes].addData(instructionsStack.pop(), i);
+				}
 			}
-			startAddressInBytes += 4;
+			
+			startAddressInBytes += this.blockSizeInBytes;
 		}
 	}
-
+	
 	public Block readBlock(int addressInBytes) {
 		return this.blocks[(int)addressInBytes/4];
 	}
 
 	public static void main(String[] args) {
-		MainMemory memory = new MainMemory(12);
+		MainMemory memory = new MainMemory(16);
 		String[] instructions = { "ADD R2, R4, R5", "SUB R9, R8, R7", "MUL R2, R4, R4", "DIV R0, R1, R5", "ADDI R2, R2, 45" };
 		
 		memory.readProgram(instructions, 0);
 		for (int i = 0; i < instructions.length; i++) {
-			System.out.println(memory.readBlock(i*4));
+			System.out.println(memory.readBlock(i * 4));
 		}
 	}
 }
