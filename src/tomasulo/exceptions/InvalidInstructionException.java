125148893f2518ae0ae905509fcf81810dc4ffbd
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2016-11-21 16:45:13 +0200

    Add InvalidInstructionException class

---
+++ src/tomasulo/exceptions/InvalidInstructionException.java
@@ -0,0 +1,15 @@
+package tomasulo.exceptions;
+
+public class InvalidInstructionException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidInstructionException() {
+		super();
+	}
+
+	public InvalidInstructionException(String message) {
+		super(message);
+	}
+
+}
