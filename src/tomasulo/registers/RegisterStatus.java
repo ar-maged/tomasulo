ef71c7c40bea9a3b96b74be92941b92c6e5e3aae
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2016-11-21 11:42:33 +0200

    RegisterBuffer -> RegisterStatus

---
+++ src/tomasulo/registers/RegisterStatus.java
@@ -0,0 +1,26 @@
+package tomasulo.registers;
+
+import java.util.Arrays;
+
+public class RegisterStatus {
+
+	private Integer[] registersEntries;
+
+	public RegisterStatus() {
+		registersEntries = new Integer[8];
+		Arrays.fill(registersEntries, null);
+	}
+
+	public Integer getROBEntryIndex(int registerIndex) {
+		return registersEntries[registerIndex];
+	}
+
+	public void setROBEntryIndex(int registerIndex, int robEntryIndex) {
+		registersEntries[registerIndex] = robEntryIndex;
+	}
+
+	public void clearROBEntryIndex(int registerIndex) {
+		registersEntries[registerIndex] = null;
+	}
+
+}
