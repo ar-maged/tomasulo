c25d83cc52a14f2b077ee8af237ee393596aea1a
ahmadnazih <a7mednazih1994@gmail.com>
2016-11-25 18:55:43 +0200

    Update InstructionBuffer.java

--- src/tomasulo/instructions/InstructionBuffer.java
+++ src/tomasulo/instructions/InstructionBuffer.java
@@ -1,5 +1,44 @@
 package tomasulo.instructions;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 
 public class InstructionBuffer {
+ 
+			
+	
+
+	 private ArrayList<Instruction>instBuffer;
+	int size;
+	
+	public  InstructionBuffer(int size){
+	 instBuffer=new ArrayList <Instruction>();
+	 this.size = size;
+	}
+
+	
+	public void Insert(Instruction [] instArray){
+		
+		for(int i=0; i<instArray.length ; i++){
+			if(instBuffer.size() < this.size)
+				instBuffer.add(instArray[i]);
+			else
+				break;
+		}
+		
+	}
+
+	
+	public  Instruction remove(){
+		
+		Instruction inst=instBuffer.remove(0);
+		
+		return inst;
+		
+	}
+	
+	
+	
 
 }
