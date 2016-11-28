e4de66b558fb524a254b55bb31a0dfb912640c76
YasmeenWafa <yasmeen_wafa@hotmail.com>
2016-11-28 11:45:08 +0200

    added instances of memory entry object & getters/setters

--- src/tomasulo/data/MemoryEntry.java
+++ src/tomasulo/data/MemoryEntry.java
@@ -1,5 +1,23 @@
 package tomasulo.data;
 
 public class MemoryEntry {
+	
+	Integer memoryLocation;
+	Integer [] data;
+	
+	
+	public Integer getMemoryLocation() {
+		return memoryLocation;
+	}
+	public void setMemoryLocation(Integer memoryLocation) {
+		this.memoryLocation = memoryLocation;
+	}
+	public Integer[] getData() {
+		return data;
+	}
+	public void setData(Integer[] data) {
+		this.data = data;
+	}
+	
 
 }
